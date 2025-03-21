d9aeea269aa4bb02add27f2cf454ad008160a916
makcim45 <super.mak4510@yandex.ru>
2025-03-21 15:01:32 +0500

    Hello World

---
+++ AngryBirdsTask.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace AngryBirds;
+
+public static class AngryBirdsTask
+{
+    public static double FindSightAngle(double v, double distance)
+    {
+        double g = 9.8;
+        return Math.PI / 4;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(PI / 4);
+    }
+}
